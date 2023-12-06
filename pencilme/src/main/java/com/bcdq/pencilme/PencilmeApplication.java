4a9dc71309297b99dcbbcba615502a2b75d58c2a
Juser0 <justuser0129@gmail.com>
2023-12-06 18:36:26 +0900

    [Chore] Set up Spring boot 2.7.17

---
+++ pencilme/src/main/java/com/bcdq/pencilme/PencilmeApplication.java
@@ -0,0 +1,13 @@
+package com.bcdq.pencilme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PencilmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PencilmeApplication.class, args);
+	}
+
+}
---
+++ pencilme/src/test/java/com/bcdq/pencilme/PencilmeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bcdq.pencilme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PencilmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
