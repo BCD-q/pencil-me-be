6338d5800b05ab6a120b5428be8b6124f226bd67
Juser0 <justuser0129@gmail.com>
2023-12-06 18:54:24 +0900

    [Feat] Implement Member, Todo Entity

---
+++ pencilme/src/main/java/com/bcdq/pencilme/member/domain/Member.java
@@ -0,0 +1,34 @@
+package com.bcdq.pencilme.member.domain;
+
+import com.bcdq.pencilme.todo.domain.Todo;
+import lombok.Builder;
+import lombok.Getter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Table(name = "member_tb")
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
+    private List<Todo> TodoList = new ArrayList<>();
+
+    @Builder
+    public Member(Long id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+}
---
+++ pencilme/src/main/java/com/bcdq/pencilme/todo/domain/Todo.java
@@ -0,0 +1,31 @@
+package com.bcdq.pencilme.todo.domain;
+
+import com.bcdq.pencilme.member.domain.Member;
+import lombok.Builder;
+import lombok.Getter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Table(name = "todo_tb")
+public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "todo_id")
+    private Long id;
+
+    private String contents;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @Builder
+    public Todo(Long id, String contents, Member member) {
+        this.id = id;
+        this.contents = contents;
+        this.member = member;
+    }
+}
