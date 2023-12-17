a6f3a14ac39a60cd4780f1ba484be5540668748e
Juser0 <justuser0129@gmail.com>
2023-12-17 20:51:14 +0900

    [Feat] Implement CommonResponse

---
+++ pencilme/src/main/java/com/bcdq/pencilme/common/CommonResponse.java
@@ -0,0 +1,33 @@
+package com.bcdq.pencilme.common;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class CommonResponse<T> {
+
+    private String responseCode;
+    private String responseMessage;
+    private T data;
+
+    @Builder
+    private CommonResponse(String responseCode, String responseMessage, T data) {
+        this.responseCode = responseCode;
+        this.responseMessage = responseMessage;
+        this.data = data;
+    }
+
+    public static CommonResponse from (final String responseCode) {
+        return CommonResponse.builder()
+                .responseCode(responseCode)
+                .build();
+    }
+    public static <T> CommonResponse<T> from (final String responseCode, final T data) {
+        return CommonResponse.<T>builder()
+                .responseCode(responseCode)
+                .data(data)
+                .build();
+    }
+}
